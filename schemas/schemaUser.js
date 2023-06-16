2882530c023891db03f8abe8571ab4bf70f558c0
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-06-16 15:50:09 +0300

    Update schemaUser.js

--- schemas/schemaUser.js
+++ schemas/schemaUser.js
@@ -2,20 +2,25 @@ const Joi = require("joi");
 
 const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-const registrationSchema = Joi.object({
+const register = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
 
-const loginSchema = Joi.object({
+const login = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
 
+const subscription = Joi.object({
+  subscription: Joi.string().valid("starter", "pro", "business").required(),
+});
+
 const schemas = {
-  registrationSchema,
-  loginSchema,
+  register,
+  login,
+  subscription,
 };
 
 module.exports = schemas;
